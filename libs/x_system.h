18e7e5492194145cc850774fab91c8f6bbd05245
Gustavo Santos Ferreira <gustavosantosfe@gmail.com>
2017-05-12 08:58:37 -0300

    created a x_system lib with sleep function
    it'll be used from now on to make pseudo cross system functions
    with preprocessor directives

---
+++ libs/x_system.h
@@ -0,0 +1,11 @@
+#ifndef _X_SYSTEM_LIB_
+#define _X_SYSTEM_LIB_
+    #ifdef WIN32
+        #include <windows.h>
+        #define x_sleep(ms) Sleep(ms)
+    #else
+        #include <unistd.h>
+        #define x_sleep(ms) usleep(ms)
+    #endif
+
+#endif
