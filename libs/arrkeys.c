539491ceaeb90fae428b4e6b8875a353ac4729f5
Gustavo Santos Ferreira <gustavosantosfe@gmail.com>
2017-05-16 10:58:18 -0300

    replaced conio's getch with curses getch on arrkeys lib, also added a case for ESC key

--- libs/arrkeys.c
+++ libs/arrkeys.c
@@ -1,26 +1,26 @@
 #include "arrkeys.h"
 
 char arrkeys(){
-    int ch = getch();
-    if (ch == 0 || ch == 224){
-        switch (_getch ()){
-            case 72:
-                return 'u';
-                break;
-            case 77:
-                return 'r';
-                break;
-            case 75:
-                return 'l';
-                break;
-            case 80:
-                return 'd';
-                break;
-            default:
-                return 'e';
-        }
-    }
-    if(ch == 13){
-        return 'q';
+    switch (getch()){
+        case 259:
+            return 'u';
+            break;
+        case 261:
+            return 'r';
+            break;
+        case 260:
+            return 'l';
+            break;
+        case 258:
+            return 'd';
+            break;
+        case 10:
+            return 'q';
+            break;
+        case 27:
+            return 'x';
+            break;
+        default:
+            return 'e';
     }
 }
